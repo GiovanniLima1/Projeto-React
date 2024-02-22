66a5633fb858c3093f5774d22cd732777aea45b5
Giovanni <giovannilima32@gmail.com>
2024-02-21 23:31:28 -0300

    Atividade prática - Desenvolvendo o Front end com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import "./Home.css"
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Chanceler</h1>
+            <img className="img" src="https://i.ytimg.com/vi/ZOeS9zZMjfY/maxresdefault.jpg"
+             alt="Palpatine" />
+        </>
+
+    )
+
+}
+
+export default Home;
